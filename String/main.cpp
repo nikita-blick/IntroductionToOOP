a020ededd02ef9e57532995e718590ed1826ab23
Nikita <Nikita@DESKTOP-13IEN5T>
2025-07-16 21:50:53 +0500

    Subscript operator in String DONE

--- String/main.cpp
+++ String/main.cpp
@@ -55,6 +55,32 @@ public:
 		size = 0;
 		cout << "Destructor:\t\t" << this << endl; 
 	}
+	//     Operators:
+	String& operator=(const String& other)
+	{
+		// this->str = other.str;  // shallow copy - поверностное копирование
+		
+		// 0) проверяем, не является ли тот объект этим объектом:
+		if (this == &other)return *this;
+		// 1) удаляем старую динамическую память 
+		delete[] this->str;
+		//Deep copy - побитовое копирование:
+		this->size = other.size;
+		// 2) Выделяем новую динамическую память:
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyAssigment:\t\t" << this << endl;
+		return *this;
+	}
+	char operator[](int i)const
+	{
+		return str[i];
+	}
+	char& operator[](int i)
+	{
+		return str[i];
+	}
+
 
 	//        Methods:
 	void print()const
@@ -71,9 +97,11 @@ String operator+(const String& left, const String& right)
 {
 	String result(left.get_size() + right.get_size() - 1);
 	for (int i = 0; i < left.get_size(); i++)
-		result.get_str()[i] = left.get_str()[i];
+		result[i] = left[i];
+		//result.get_str()[i] = left.get_str()[i];
 	for (int i = 0; i < right.get_size(); i++)
-		result.get_str()[i + left.get_size() - 1] = right.get_str()[i];
+		result[i + left.get_size() - 1] = right[i];
+		//result.get_str()[i + left.get_size() - 1] = right.get_str()[i];
 	return result;
 }
 std::ostream& operator<<(std::ostream& os, const String& obj)
@@ -81,8 +109,8 @@ std::ostream& operator<<(std::ostream& os, const String& obj)
 	return os << obj.get_str();
 }
 
-//#define CONSTRUCTORS_CHECK
-
+#define CONSTRUCTORS_CHECK
+//#define COPY_SEMANTIC_CHECK
 
 
 void main()
@@ -92,27 +120,31 @@ void main()
 
 	String str1;
 	str1.print();
-	cout << delimiter << endl;
 	String str2(5);      // Conversion from 'int' to 'String'
 	str2.print();
-	cout << delimiter << endl;
 	String str3 = "Hello";
 	str3.print();
 	cout << str3 << endl;
-	cout << delimiter << endl;
 	String str4 = "World";
 	cout << str4 << endl;
 	cout << delimiter << endl;
 	String str5 = str3 + str4;
+	cout << delimiter << endl;
 	cout << str5 << endl;
 
 #endif // CONSTRUCTORS_CHECK
 
+#ifdef COPY_SEMANTIC_CHECK
+
 	String str1 = "Hello";
+	str1 = str1;
 	cout << str1 << endl;
 
 	String str2 = str1;
+	str2 = str1;
 	cout << str2 << endl;
+
+#endif // DEBUG
 
 
 }
